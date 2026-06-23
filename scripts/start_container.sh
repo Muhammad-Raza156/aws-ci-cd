031d0bbf104eea5c7c56c28fff6c3827c5499d8c
Muhammad Raza <hafizrazam@gmail.com>
2026-06-23 20:03:27 +0500

    Fix formatting in start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull hmraza/simple-python-flask-app
-
+ 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 hmraza/simple-python-flask-app
